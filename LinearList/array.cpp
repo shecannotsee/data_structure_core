61f663aecc8f3867bb260c4c7c797ef1161374eb
shecannotsee <shecannotsee@163.com>
2020-09-28 15:32:17 +0800

    1.完成数组类的功能和设计上的修改。 2.测试修改

--- LinearList/array.cpp
+++ LinearList/array.cpp
@@ -4,16 +4,20 @@ using namespace she;
 
 template<class E>
 array<E>::array(){
-    m_head=new E(100);
-    m_length=100;
-    (*this).clear();
+    m_createArray();
 }
 
 template<class E>
-array<E>::array (int length){
-    m_head=new E(length);
-    m_length=length;
-    (*this).clear();
+array<E>::array(int length){
+    m_createArray(length);
+}
+
+template<class E>
+array<E>::array(int length,const E& element){
+    m_createArray(length);
+    for(int i=0;i<m_length;i++){
+        *(m_head+i)=element;
+    }
 }
 
 template<class E>
@@ -23,55 +27,48 @@ array<E>::~array(){
 }
 
 template<class E>
-void array<E>::clear(){
-    for(int i=0;i<m_length;i++){
-        *(m_head+i)=0;
-    }
-}
-
-template<class E>
-bool array<E>::empty() const{
-    if(m_length!=0){
-        return false;
-    }
-    else{//m_length==0
-        return true;
-    }
-}
-
-template<class E>
 int array<E>::length() const{
     return m_length;
 }
 
-
-
 template<class E>
-E& array<E>::get(int i) const{
-    if(i<m_length){
-        return *(m_head+i);
+E& array<E>::operator[](int position) const{
+    if(position<m_length){
+        return *(m_head+position);
     }
     else{
-        errorPrintf("Refused to visit.");
+        ERRORPRINTF("error:Array access violation.\n");
+        return *(m_head+position);
     }
 }
 
 template<class E>
-void array<E>::set(int i,E element){
-    if(i<m_length){
-        *(m_head+i)=element;
+E& array<E>::get(int position) const{
+    if(position<m_length){
+        return *(m_head+position);
     }
     else{
-        errorPrintf("Refused to visit.");
+        ERRORPRINTF("error:Array access violation.\n");
+        return *(m_head+position);
     }
 }
 
 template<class E>
-void array<E>::insertElement(const E& element,int i){
-
+void array<E>::set(const E& element,int position){
+    if(position<m_length){
+        *(m_head+position)=element;
+    }
+    else{
+        ERRORPRINTF("error:Array access violation,refused to visit.\n");
+    }
 }
 
 template<class E>
-void array<E>::deleteElement(int i){
-    
+void array<E>::m_createArray(int length){
+    if(length<=0){
+        length=DEFAULT_LENGTH;
+        ERRORPRINTF("error:The input parameter is incorrect.\n");
+    }
+    m_head=new E(length);
+    m_length=length;
 }
--- LinearList/array.h
+++ LinearList/array.h
@@ -1,31 +1,79 @@
 #ifndef _ARRAY_H_
 #define _ARRAY_H_
 #include <stdio.h>
-#define errorPrintf printf
+#define ERRORPRINTF printf
+#define DEFAULT_LENGTH (128)
 namespace she{
 
 template<class E>
 class array{
 public:
+    /** @brief Construct an array with default length(128).
+    
+    */
     array();
+
+    /** @brief Construct an array with specified length.
+    
+    @param length The length of the array.
+    */
     array(int length);
+
+    /** @brief Construct an array with specified length and initial element.
+    
+    @param length The length of the array.
+    @param element The initial value of the array.
+    */
+    array(int length,const E& element);
+
+    /** @brief Destructor.
+    
+    Frees dynamically allocated array memory space,and set the head pointer to null.
+    */
     virtual ~array();
 
 public:
-    void clear();//Empty the array.
-    bool empty() const;//Determine whether it is empty.If it is empty then return true.
-    int length() const;//Returns the length of the array.
+    /** @brief Returns the length of the array.
+    
+    */
+    int length() const;
+
+    /** @brief Get the element of the position.
+    
+    @param position Position of element.
+    */
+    E& operator[](int position) const;
+
+    /** @brief Get the element of the position.
+    
+    @param position Position of element.
+    */
+    E& get(int position) const;
 
 public:
-
-    E& get(int i) const;//get the value of the i position.
-    void set(int i,E element);//set the value of the i position.
-    void insertElement(const E& element,int i);//Insert the element.
-    void deleteElement(int i);//Delete the i element.
+    /** @brief Set the element of the position.
+    
+    @param position Position of element.
+    @param element Elements that need to be modified.
+    */
+    void set(const E& element,int position);
 
 private:
-    E* m_head;//array pointer.
-    int m_length;//Array length.
+    /** @brief array pointer.
+    
+    */
+    E* m_head;
+    
+    /** @brief Array length.
+    
+    */
+    int m_length;
+
+    /** @brief Construct and initialize the array.
+    
+    @param length The length of array(default length is 128).
+    */
+    void m_createArray(int length=DEFAULT_LENGTH);
 
 };//class array
 
