8a65419d8b5554bf2fb7d76a9df79de748d7a98d
shecannotsee <shecannotsee@163.com>
2023-03-17 14:12:30 +0800

    feat:添加跳表的架构

---
+++ src/linear/skip_list.h
@@ -0,0 +1,12 @@
+//
+// Created by shecannotsee on 2023/3/17.
+//
+
+#ifndef DATA_STRUCTURE_CORE_SRC_LINEAR_SKIP_LIST_H_
+#define DATA_STRUCTURE_CORE_SRC_LINEAR_SKIP_LIST_H_
+
+class skip_list {
+
+};
+
+#endif //DATA_STRUCTURE_CORE_SRC_LINEAR_SKIP_LIST_H_
