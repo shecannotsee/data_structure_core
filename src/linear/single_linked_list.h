af33c17f45232bb9cca40c9111b5724524e9219f
shecannotsee <shecannotsee@163.com>
2023-03-20 23:59:55 +0800

    feat:添加单链表demo的简单框架

--- src/linear/single_linked_list.h
+++ src/linear/single_linked_list.h
@@ -5,8 +5,40 @@
 #ifndef DATA_STRUCTURE_CORE_SRC_LINEAR_SINGLE_LINKED_LIST_H_
 #define DATA_STRUCTURE_CORE_SRC_LINEAR_SINGLE_LINKED_LIST_H_
 
-class single_linked_list {
+#include <memory>
 
-};
+namespace she {
+
+template<typename T>
+class single_linked_list {
+ private:
+  struct node {
+    T value{};
+    std::unique_ptr<node> next{nullptr};
+  };
+ public:
+  single_linked_list();
+
+ private:
+  std::unique_ptr<node> head_ptr_;
+  std::unique_ptr<node> tail_ptr_;
+  int size_;
+
+ public:
+  // insert from tail
+  bool add(T&& element);
+  // insert from scratch
+  bool insert(T&& element);
+  // delete current node
+  bool remove(node* p);
+
+ public:
+  int size() const;
+  node* begin() const;
+  node* end() const;
+
+};// class single_linked_list
+
+};// namespace she
 
 #endif //DATA_STRUCTURE_CORE_SRC_LINEAR_SINGLE_LINKED_LIST_H_
