aa430a5b53161e82365ed04340df1c2c6872b487
shecannotsee <shecannotsee@163.com>
2023-09-08 18:08:32 +0800

    [test] 1.root insert

--- test/tree/test__binary_tree.h
+++ test/tree/test__binary_tree.h
@@ -5,4 +5,17 @@
 #ifndef DATA_STRUCTURE_CORE_TEST__BINARY_TREE_H
 #define DATA_STRUCTURE_CORE_TEST__BINARY_TREE_H
 
+#include <tree/binary_tree.h>
+
+TEST(binary_tree,base_test) {
+  she::binary_tree<int> entity;
+  entity.insert_root(1);/* test verification */{
+    auto root = entity.get_root();
+    EXPECT_EQ(root->data, 1);
+    entity.insert_this_node_left(root,1);
+  }
+
+
+}
+
 #endif //DATA_STRUCTURE_CORE_TEST__BINARY_TREE_H
